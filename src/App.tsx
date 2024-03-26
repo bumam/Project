4eadc07033ef8be119042ce0cbe7499f69b3fb96
Mariia Boldyreva <60100210+bumam@users.noreply.github.com>
2024-03-26 18:03:07 +0300

    css themes and useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import "./index.scss";
-import Counter from "./components/Counter/counter";
+import React, { Suspense } from "react";
+import "./styles/index.scss";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/aboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/mainPage.async";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div>
-      123
-      <Counter />
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/about"}> About</Link>
+      <Link to={"/"}>Main</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
